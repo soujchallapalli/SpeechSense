e0afab502cd5a5d9eb88255df8ff2d011893eac6
Rania Daoud <rania.a.dawod@gmail.com>
2026-06-07 10:44:23 +0100

    Add initial mock function for parallelism
    Sets up the initial architecture for parallel execution.
    Input structure is can change in future iterations.

--- src/speechsense/main.py
+++ src/speechsense/main.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterator
+
+
 def foo(bar: str) -> str:
     """Summary line.
 
@@ -13,5 +16,49 @@ def foo(bar: str) -> str:
     return bar
 
 
+# processess the whole file and returns the whole CSV
+def record_to_file() -> str:
+    print("csv is initialized")
+    return "tests/mock_data.csv"
+
+
+# Correct the Transcript With AI
+def sanitize_with_AI(row: list):
+    print("AI processing done")
+    return
+
+
+# how many times this speaker talked so far
+def add_speaker_counter():
+    print("speaker counter added")
+    return
+
+
+# Enrich the Dataset With Python
+def analyze(row: Iterator):
+    print("data is analyised with python")
+    return
+
+
+# Validate the CSV
+def validate(row: Iterator):
+    print("CSV is validated")
+    return
+
+
+## all the parllel processing
+def process(row: Iterator):
+    sanitize_with_AI(row)
+    validate(row)
+    analyze(row)
+
+    return
+
+
 if __name__ == "__main__":  # pragma: no cover
-    pass
+    csv_path = record_to_file()
+
+    with open(csv_path) as f:
+        it = iter(f)
+        for row in it:
+            process(row)
