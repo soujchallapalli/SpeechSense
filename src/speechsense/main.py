8e3bb6778f1f9bd241ccb10375cd3e9083a25586
Rania Daoud <rania.a.dawod@gmail.com>
2026-06-07 14:07:05 +0100

    cleaning up and adding the processing in a separate file
    We are still dealing with the main bottleneck
    * race condition in writing that might ruin the timestamps order and also corrupt data
    * num of workers is randomly added fro now, it needs to rely on the gemini API calls rate limit
    * [stretch goal] split the audio file and process it faster in parallel as well

--- src/speechsense/main.py
+++ src/speechsense/main.py
@@ -1,6 +1,4 @@
-from collections.abc import Iterator
-from concurrent.futures import ThreadPoolExecutor
-from time import sleep
+from speechsense.pipeline import process
 
 
 def foo(bar: str) -> str:
@@ -18,53 +16,5 @@ def foo(bar: str) -> str:
     return bar
 
 
-# processess the whole file and returns the whole CSV
-def record_to_file() -> str:
-    print("csv is initialized")
-    return "tests/mock_data.csv"
-
-
-# Correct the Transcript With AI
-def sanitize_with_AI(row: list):
-    sleep(3)
-    print("AI processing done")
-    return
-
-
-# how many times this speaker talked so far
-def add_speaker_counter():
-    sleep(3)
-    print("speaker counter added")
-    return
-
-
-# Enrich the Dataset With Python
-def analyze(row: Iterator):
-    sleep(3)
-    print("data is analyised with python")
-    return
-
-
-# Validate the CSV
-def validate(row: Iterator):
-    sleep(3)
-    print("CSV is validated")
-    return
-
-
-## all the parllel processing
-def process(row: Iterator):
-    sanitize_with_AI(row)
-    validate(row)
-    analyze(row)
-    return
-
-
 if __name__ == "__main__":  # pragma: no cover
-    csv_path = record_to_file()
-
-    with open(csv_path) as f:
-        it = iter(f)
-        with ThreadPoolExecutor(max_workers=15) as executor:
-            results = list(executor.map(process, it))
-        add_speaker_counter()
+    process()
